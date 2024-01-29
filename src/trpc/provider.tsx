015995e24e0920b26a175e2274b41ff862a3f3d6
jeffjuann <jeffjuann@gmail.com>
2024-01-30 01:40:19 +0700

    fix(cache): add header to provider

--- src/trpc/provider.tsx
+++ src/trpc/provider.tsx
@@ -13,6 +13,9 @@ export default function TrpcProvider({ children }: { children: React.ReactNode }
       links: [
         httpBatchLink({
           url: "http://localhost:3000/api/trpc",
+          headers: {
+            'cache-control': `s-maxage=1, stale-while-revalidate=1`,
+          },    
         }),
       ],
     })
