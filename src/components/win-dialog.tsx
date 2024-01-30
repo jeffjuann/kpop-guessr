1576e64fc3f95497ee1b0766893f9c960acad1fe
Jefferson Juan <55904713+jeffjuann@users.noreply.github.com>
2024-01-30 17:13:01 +0700

    fix(dialog): fix dialog load state (#11)

--- src/components/win-dialog.tsx
+++ src/components/win-dialog.tsx
@@ -22,7 +22,7 @@ export default function WinDialog({ isOpen, guessCount }: { isOpen: boolean, gue
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">You Win !!!</h2>
         <p>Congratulations, You have successfully guessed correctly after {guessCount} attempts.</p>
         <DialogFooter>
-          <Button onClick={() => playAgain()} className="btn btn-primary">Play Again</Button>
+          <Button onClick={() => playAgain()} className="btn btn-primary" disabled={btnDisabled}>Play Again</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
